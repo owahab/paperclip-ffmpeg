d3c5525c0ec1d5f4bab73d9029549aa40e528b2f
nifuramu <nozzzzz@gmail.com>
2014-03-27 10:47:30 +0900

    Fix test

--- spec/dummy/app/models/video.rb
+++ spec/dummy/app/models/video.rb
@@ -1,6 +1,6 @@
 class Video < ActiveRecord::Base
 
-	has_attached_file :clip, :styles => {
+  has_attached_file :clip, :styles => {
     :medium => { :geometry => "640x480", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} }
   }, :processors => [:ffmpeg]
@@ -33,6 +33,13 @@ class Video < ActiveRecord::Base
   	:large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :thumb => { :geometry => "100x100#", :format => 'foo', :time => 0}
   }, :processors => [:ffmpeg]
+
+  validates_attachment_content_type :clip, content_type:["video/mp4"]
+  validates_attachment_content_type :wrongClip, content_type:["video/mp4"]
+  validates_attachment_content_type :clip_thumb_exceed, content_type:["video/mp4"]
+  validates_attachment_content_type :clip_thumb_normal, content_type:["video/mp4"]
+  validates_attachment_content_type :clip_thumb_negative, content_type:["video/mp4"]
   validates_attachment_content_type :clip_thumb_bad_extension, content_type:['video/mp4','video/mpeg']
+
 end
 
