10a0d8d12fa1ec2844149aceda99e209967286e8
vincent@teqqi.ca <vincent@teqqi.ca>
2014-06-18 11:29:07 -0400

    Fix for Tests to run on Travis.

--- paperclip-ffmpeg.gemspec
+++ paperclip-ffmpeg.gemspec
@@ -21,7 +21,7 @@ Gem::Specification.new do |s|
 
   s.add_dependency("rails")
   s.add_dependency('paperclip', '>=2.5.2')
-  s.add_development_dependency('rspec-rails')
+  s.add_development_dependency('rspec-rails', '~> 2.14.2')
   s.add_development_dependency("sqlite3")
   s.add_development_dependency("database_cleaner")
   s.add_development_dependency("shoulda")
--- spec/dummy/app/models/video.rb
+++ spec/dummy/app/models/video.rb
@@ -4,30 +4,35 @@ class Video < ActiveRecord::Base
     :medium => { :geometry => "640x480", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} }
   }, :processors => [:ffmpeg]
+  validates_attachment_content_type :clip, content_type:['video/mp4','video/mpeg']
 
   has_attached_file :wrongClip, :styles => {
     :large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 3000}} }
   }, :processors => [:ffmpeg]
+  validates_attachment_content_type :wrongClip, content_type:['video/mp4','video/mpeg']
 
   has_attached_file :clip_thumb_exceed, :styles => {
     :large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10}
   }, :processors => [:ffmpeg]
+  validates_attachment_content_type :clip_thumb_exceed, content_type:['video/mp4','video/mpeg']
 
   has_attached_file :clip_thumb_normal, :styles => {
     :large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :thumb => { :geometry => "100x100#", :format => 'jpg', :time => 0}
   }, :processors => [:ffmpeg]
+  validates_attachment_content_type :clip_thumb_normal, content_type:['video/mp4','video/mpeg']
 
   has_attached_file :clip_thumb_negative, :styles => {
   	:large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :thumb => { :geometry => "100x100#", :format => 'jpg', :time => -10}
   }, :processors => [:ffmpeg]
-
+  validates_attachment_content_type :clip_thumb_negative, content_type:['video/mp4','video/mpeg']
+  
   has_attached_file :clip_thumb_bad_extension, :styles => {
   	:large => { :geometry => "1024x576", :format => 'flv', :convert_options => {:output => {:ar => 44100}} },
     :thumb => { :geometry => "100x100#", :format => 'foo', :time => 0}
   }, :processors => [:ffmpeg]
-
+  validates_attachment_content_type :clip_thumb_bad_extension, content_type:['video/mp4','video/mpeg']
 end
 
